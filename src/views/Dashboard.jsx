e7a3e677245196f35fe4b5da20e7a8359587c0f9
Chris <christhehorizon@gmail.com>
2022-01-12 16:22:51 -0800

    Added useEffect hook to fetch API

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Dashboard = () => {
+    const [title, setTitle] = useState({});
+
+    useEffect(() => {
+        fetch("https://www.freetogame.com/api/games")
+            .then(res => res.json())
+            .then(jsonRes => console.log(jsonRes))
+            .catch(err => console.log(err))
+    })
+
     return (
         <fieldset>
             <legend>Dashboard</legend>
