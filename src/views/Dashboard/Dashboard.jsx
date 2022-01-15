7520df94e48b08345df04e903ab39901ac15b8c5
Chris <christhehorizon@gmail.com>
2022-01-14 16:08:58 -0800

    Added game URL to game card

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -21,20 +21,22 @@ const Dashboard = () => {
                     return <div key={i}>
                         <div className='allGames__container'>
                             <div className='allGames__container-card'>
-                                <div className="allGames__container-card_img">
-                                    <img src={game.thumbnail} alt="image" height={200} width={250} />
-                                    <div className="allGames__container-card_title">
-                                        <h2>{game.title}</h2>
-                                        <p>Free</p>
-                                        <div className="allGames__container-card_description">
-                                            <p>{game.short_description}</p>
-                                            <div className="allGames__container-card_genre">
-                                                <p>{game.genre}</p>
-                                                <p>{game.platform}</p>
+                                <a href={game.game_url} className='allGames__container-url'>
+                                    <div className="allGames__container-card_img">
+                                        <img src={game.thumbnail} alt="image" height={200} width={250} />
+                                        <div className="allGames__container-card_title">
+                                            <h2>{game.title}</h2>
+                                            <p>Free</p>
+                                            <div className="allGames__container-card_description">
+                                                <p>{game.short_description}</p>
+                                                <div className="allGames__container-card_genre">
+                                                    <p>{game.genre}</p>
+                                                    <p>{game.platform}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </a>
                             </div>
                         </div>
                     </div>
